c081f07bcb070d8b386a1db9511f97bb85605d49
kamilKoo <kamiekurat98@o2.pl>
2022-07-11 23:44:14 +0200

    Created a User. Added Hashed and password confirmed.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,28 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { RegisterDto } from "./dto/register.dto";
+import { UserService } from "../user/user.service";
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
 
+  constructor(private userService: UserService) {
+  }
+
   @Post('admin/register')
-  register(@Body() body: RegisterDto){
-      return body;
+  async register(@Body() body: RegisterDto){
+    const {passwordConfirm, ...data} = body;
+
+    if (body.password !== body.passwordConfirm) {
+      throw new BadRequestException('Password do not match!');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.save({
+      ...data,
+      password: hashed,
+      isServisant: false,
+    });
   }
 }

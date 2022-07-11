f1df84251c88b85fbb18963429f59a55b9e3e2f7
kamilKoo <kamiekurat98@o2.pl>
2022-07-11 21:13:05 +0200

    Added validation to register.

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,7 +1,15 @@
-export interface RegisterDto {
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class RegisterDto {
+  @IsNotEmpty()
   firstName: string;
+  @IsNotEmpty()
   lastName: string;
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
   password: string;
+  @IsNotEmpty()
   passwordConfirm: string;
 }

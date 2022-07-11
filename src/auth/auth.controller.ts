242b48e6132e27c1098f60ae87702b8d4feaa27a
kamilKoo <kamiekurat98@o2.pl>
2022-07-11 20:48:31 +0200

    Create authentication dto register.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { RegisterDto } from "./dto/register.dto";
+
+@Controller()
+export class AuthController {
+
+  @Post('admin/register')
+  register(@Body() body: RegisterDto){
+      return body;
+  }
+}

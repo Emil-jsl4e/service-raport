fe2fdca6a91b065e192f7ab716f568f7c7e35a46
kamilKoo <kamiekurat98@o2.pl>
2022-07-13 09:08:42 +0200

    Added get all servisants

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, UseInterceptors } from "@nestjs/common";
+import { UserService } from "./user.service";
 
-@Controller('user')
-export class UserController {}
+@Controller()
+@UseInterceptors(ClassSerializerInterceptor)
+export class UserController {
+
+  constructor(private readonly userService: UserService) {
+  }
+  //Servisants Technical
+  @Get('admin/servisants')
+  async servisants(){
+    return this.userService.find({where:{
+      isServisant: true
+    }});
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,10 +13,13 @@ export class UserService {
   async save(options) {
     return this.userRepository.save(options);
   }
+  async find(options) {
+    return this.userRepository.find(options);
+  }
   async findOne(options) {
     return this.userRepository.findOne(options);
   }
-  async update(id: number, option){
-    return this.userRepository.update(id, option);
+  async update(id: number, options){
+    return this.userRepository.update(id, options);
   }
 }

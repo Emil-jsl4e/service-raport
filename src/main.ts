63793f3e1315885bc137e7c1039437ba2dc48584
Emil <kamiekurat98@o2.pl>
2022-08-29 22:20:54 +0200

    Added simply validation.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
--- src/service-orders/service-orders-activate.controller.ts
+++ src/service-orders/service-orders-activate.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, NotFoundException, Post } from "@nestjs/common";
 import { ServiceOrderActivate } from "./serviceOrder-activate";
 import { ServiceOrderActivateService } from "./serviceOrder-activate.service";
+import { ServiceOrdersService } from "./service-orders.service";
 
 @Controller()
 export class ServiceOrdersActivateController {
   constructor(
-    private serviceOrderActivate: ServiceOrderActivateService
+    private serviceOrderActivate: ServiceOrderActivateService,
+    private serviceOrdersService: ServiceOrdersService
   ) {
   }
 
@@ -17,12 +19,29 @@ export class ServiceOrdersActivateController {
      @Post('admin/ordersActive')
      async register(@Body() body: ServiceOrderActivate){
      const {servisantFirstName, servisantLastName, description, serviceOrders } = body;
+     //   try {
+     // const id = serviceOrders;
+     //   const user = await this.serviceOrdersService.find({where: {id}});
+     //   console.log(user);
+     //
+     //   }
+     //   catch (e) {
+     //     return e.message
+     //   }
+       const id = serviceOrders;
+       if (await this.serviceOrdersService.findOne({where: {id}})) {
+         console.log("Id is ok ");
+       }
+       throw new NotFoundException('Id is not found');
 
-     return this.serviceOrderActivate.save({
-       servisantFirstName: servisantFirstName,
-       servisantLastName: servisantLastName,
-       description: description,
-       serviceOrders: serviceOrders,
-    });
+       //TODO dodac walidacje ta nie działa sprawdzania czy zadanie z innej listy istnieje
+
+    //  return this.serviceOrderActivate.save({
+    //    servisantFirstName: servisantFirstName,
+    //    servisantLastName: servisantLastName,
+    //    description: description,
+    //    serviceOrders: serviceOrders,
+    // });
   }
+
 }
